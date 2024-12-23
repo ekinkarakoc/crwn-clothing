eeadb2d169b9cd875b7f38d15a2e4a8b131c14fb
Ekin KARAKOÇ <ekinkarakoc1@gmail.com>
2024-12-23 12:10:14 +0300

    adding-sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
-
 import "./App.css";
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <>
-      <p>Hello World!</p>
-    </>
+    <div className="categories-container">
+      {categories.map(({ title, id }) => {
+        return (
+          <div className="category-container" key={id}>
+            <div className="background-image" />
+            <div className="category-cody-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
